70a507faa133b919726692a6ec9844b66ee545a5
DeNic0la <nicola@fioretti.info>
2022-05-26 10:47:24 +0200

    removed console log

--- movie/controller.js
+++ movie/controller.js
@@ -3,7 +3,7 @@ import { render } from './view.js';
 import { render as form } from './form.js';
 
 export async function listAction(request, response) {
-    const data = await getAll(request.user.id).catch(reason => console.log(reason));
+    const data = await getAll(request.user.id);
     const body = render(data);
     response.send(body);
 }
