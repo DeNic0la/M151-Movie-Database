14cc4b1ca8d697818c64b945f739ff5331eaf8bf
codefactor-io <support@codefactor.io>
2022-06-11 11:25:05 +0000

    [CodeFactor] Apply fixes

--- model/model.js
+++ model/model.js
@@ -5,23 +5,23 @@ import {Movies} from "./movie.js";
 
 Movies.hasMany(Ratings,{
     foreignKey: 'movie'
-})
+});
 Movies.belongsTo(Users,{
     foreignKey:'user'
-})
+});
 
 Ratings.belongsTo(Users,{
     foreignKey: 'user',
-})
+});
 Ratings.belongsTo(Movies,{
     foreignKey: 'movie'
-})
+});
 
 Movies.belongsTo(Users,{
     foreignKey: 'user'
-})
+});
 Users.hasMany(Movies,{
     foreignKey: 'user'
-})
+});
 
-export {Ratings, Users, Movies}
+export {Ratings, Users, Movies};
