03d36f251f2eda61f5a5abc822d5d3cc10205361
DeNic0la <nicola@fioretti.info>
2022-06-15 08:24:11 +0200

    added Ratrings to view, without logic

--- movie/view.js
+++ movie/view.js
@@ -1,3 +1,20 @@
+function renderRating(amount, movieId){
+    let str = "";
+    for (let i = 1; i <= 5; i++) {
+        if (amount >= i){
+            str += `
+                <a href="TODO">★</a>
+            `
+        }
+        else {
+            str += `
+                <a href="TODO">☆</a>
+            `
+        }
+    }
+    return str;
+}
+
 export function render(movies) {
     return `
 <!DOCTYPE html>
@@ -10,7 +27,7 @@ export function render(movies) {
 <body>
 <a href="/logout">abmelden</a>
   <table>
-    <thead><tr><th>Id</th><th>Title</th><th></th><th></th></tr></thead>
+    <thead><tr><th>Id</th><th>Title</th><th>Rating</th><th>AVG</th><th></th><th></th></tr></thead>
     <tbody>
       ${movies
         .map(
@@ -18,8 +35,10 @@ export function render(movies) {
         <tr>
           <td>${movie.id}</td>
           <td>${movie.title}</td>
+          <td>${renderRating(movie.rating,movie.id)}</td> 
+          <td>${movie.avg}</td>
           <td><a href="/movie/delete/${movie.id}">löschen</a></td>
-          <td><a href="/movie/form/${movie.id}">bearbeiten</a></td> 
+          <td><a href="/movie/form/${movie.id}">bearbeiten</a></td>
         </tr>`,
         )
         .join('')}
